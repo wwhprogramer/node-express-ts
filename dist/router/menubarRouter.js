f2beb9a418364a24c32621c83a1e889c29ee8703
吴温浩 <wuwenhao@163.com>
2020-09-10 17:05:22 +0800

    [update]更新menubarRouter/getAllMenu，将对象数组封装为树

--- dist/router/menubarRouter.js
+++ dist/router/menubarRouter.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var MenubarService_1 = __importDefault(require("../service/MenubarService"));
 var menubarRouter = express_1.default.Router();
-menubarRouter.get('/selectById', function (req, res) {
+menubarRouter.get('/getAllMenu', function (req, res) {
     // const menubarObj: MenubarInterface = {
     //     id: 0,
     //     parentId: 0,
@@ -26,10 +26,46 @@ menubarRouter.get('/selectById', function (req, res) {
     var menubarService = new MenubarService_1.default();
     menubarService.getAll()
         .then(function (result) {
-        res.send(result);
+        var packageResult = packageTree(result);
+        res.send(packageResult);
     })
         .catch(function (err) {
         res.send(err);
     });
 });
+// 将menubar封装成树形组件
+var packageTree = function (menubarList) {
+    var packageList = [];
+    // 根节点
+    for (var i = 0; i < menubarList.length; i++) {
+        var item = menubarList[i];
+        if (!item.parentId) {
+            packageList.push(item);
+            menubarList.splice(i, 1);
+            i--;
+        }
+    }
+    return packageChild(menubarList, packageList);
+};
+var packageChild = function (menubarList, packageList) {
+    for (var i = 0; i < menubarList.length; i++) {
+        var menubarItem = menubarList[i];
+        for (var j = 0; j < packageList.length; j++) {
+            var packageItem = packageList[j];
+            if (packageItem.id === menubarItem.parentId) {
+                var children = packageItem.children ? packageItem.children : [];
+                children.push(menubarItem);
+                packageItem.children = children;
+                menubarList.splice(i, 1);
+                if (!menubarList.length) {
+                    return packageList;
+                }
+                i--;
+                packageItem.children = packageChild(menubarList, packageItem.children);
+                continue;
+            }
+        }
+    }
+    return packageList;
+};
 exports.default = menubarRouter;
--- router/menubarRouter.ts
+++ router/menubarRouter.ts
@@ -4,7 +4,7 @@ import MenubarService from '../service/MenubarService'
 
 const menubarRouter = express.Router()
 
-menubarRouter.get('/selectById', (req: any, res: any) => {
+menubarRouter.get('/getAllMenu', (req: any, res: any) => {
     // const menubarObj: MenubarInterface = {
     //     id: 0,
     //     parentId: 0,
@@ -23,14 +23,51 @@ menubarRouter.get('/selectById', (req: any, res: any) => {
     // const menubar = new Menubar(menubarObj)
     const menubarService = new MenubarService()
     menubarService.getAll()
-    .then((result: any) => {
-        res.send(result)
+    .then((result: any[]) => {
+        const packageResult = packageTree(result)
+        res.send(packageResult)
     })
     .catch((err: any) => {
         res.send(err)
     })
-
-
 })
+
+// 将menubar封装成树形组件
+const packageTree = (menubarList: any[]): any[] => {
+    let packageList: any[] = []
+    // 根节点
+    for(let i = 0; i < menubarList.length; i++) {
+        let item = menubarList[i]
+        if(!item.parentId) {
+            packageList.push(item)
+            menubarList.splice(i, 1)
+            i--
+        }
+    }
+    return packageChild(menubarList, packageList)
+}
+
+const packageChild = (menubarList: any[], packageList: any[]): any[] => {
+    for(let i = 0; i < menubarList.length; i++) {
+        let menubarItem = menubarList[i]
+        for(let j = 0; j < packageList.length; j++) {
+            let packageItem = packageList[j]
+            if(packageItem.id === menubarItem.parentId) {
+                let children = packageItem.children ? packageItem.children : []
+                children.push(menubarItem)
+                packageItem.children = children
+                menubarList.splice(i, 1)
+                if(!menubarList.length) {
+                    return packageList
+                }
+                i--
+                packageItem.children = packageChild(menubarList, packageItem.children)
+                continue
+            }
+        }
+    }
+    return packageList
+}
+
 
 export default menubarRouter
